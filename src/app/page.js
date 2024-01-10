fd955443829f04373f7e6373e659c397330ba82f
Miguel Sánchez <mangelsr25@gmail.com>
2024-01-09 19:40:59 -0500

    [UPD] HomePage component to display fetched data

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,37 @@
-function HomePage() {
+import { getClient } from '@/lib/client'
+import { gql } from '@apollo/client'
+
+async function loadData() {
+  const { data } = await getClient().query({
+    query: gql`
+      query {
+        characters(page: 1) {
+          results {
+            id
+            name
+            image
+          }
+        }
+      }
+    `
+  })
+  return data.characters.results
+}
+
+async function HomePage() {
+  const characters = await loadData()
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Characters...</h1>
+      {
+        characters.map(character => (
+          <div key={character.id}>
+            Name: {character.name}
+            <img src={character.image} alt={character.name} />
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
