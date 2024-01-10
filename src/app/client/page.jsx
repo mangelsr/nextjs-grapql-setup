440d9508ff9d28466cf3eed7b3bb2bd1d91f9e2c
Miguel Sánchez <mangelsr25@gmail.com>
2024-01-09 20:14:30 -0500

    [UPD] ClientPage component to consume GrahpQL directly from client component

--- src/app/client/page.jsx
+++ src/app/client/page.jsx
@@ -1,7 +1,33 @@
 "use client";
 
+import { gql } from "@apollo/client";
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+
+const query = gql`
+  query {
+    characters(page: 1) {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
 function ClientPage() {
-  return <div>ClientPage</div>;
+  const { data } = useSuspenseQuery(query);
+  console.log(data.characters.results);
+  return (
+    <div className="grid grid-cols-4">
+      {data.characters.results.map((character) => (
+        <div key={character.id}>
+          <h3>{character.name}</h3>
+          <img src={character.image} alt={character.name} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ClientPage;
